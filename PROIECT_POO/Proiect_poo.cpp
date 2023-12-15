a2f692b71b57cf280ffb4f4231d8e822c0187a21
hiroi-p <nadaseanpaul@gmail.com>
2023-12-15 11:07:25 +0200

    modified:   PROIECT_POO/Proiect_poo.cpp     Coaie

--- PROIECT_POO/Proiect_poo.cpp
+++ PROIECT_POO/Proiect_poo.cpp
@@ -4,37 +4,61 @@
 
 using namespace std;
 
-int  Meniu_principal()
-{int opt;
-    cout << "-------------------------------"<<endl;
-    cout << "Bookstore app" << endl<< endl << endl;
+int Meniu_principal()
+{
+    int opt;
+    cout << "-------------------------------" << endl;
+    cout << "Bookstore app" << endl
+         << endl
+         << endl;
     cout << "Alegeti o optiune: " << endl;
     cout << "1.Conectare" << endl;
     cout << "2.Contact" << endl;
-    cout << "-------------------------------"<<endl;
-    cin>>opt;
+    cout << "-------------------------------" << endl;
+    cin >> opt;
     switch (opt)
     {
-    case 1: Meniu_conectare();
+    case 1:
+        Meniu_conectare();
         break;
-    case 2:Meniu_contact();
+    case 2:
+        Meniu_contact();
     default:
-    Meniu_principal();
+        cout << "Optiune incorecta!   Mai alege-ti o data! " << endl;
+        Meniu_principal();
         break;
     }
-    
 }
 void Meniu_conectare()
 {
+    int opt;
+
+    cout << "Alegeti optiunea: " << endl;
     cout << "1.Inregistrare" << endl;
     cout << "2.Autentificare" << endl;
     cout << "3.Resetare Parola" << endl;
     cout << "4.Inapoi" << endl;
+    cin >> opt;
+    switch (opt)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    default:
+        Meniu_conectare();
+        break;
+    }
 }
 void Meniu_contact()
 {
+
 }
-void Meniu4()
+void Meniu_Autentificat()
 {
     cout << "1.Achizitie/Imprumut" << endl;
     cout << "2.Istoric" << endl;
